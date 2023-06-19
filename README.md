e5c0b0cd8907dab0caa6b5f6ba83b29f8d7403eb
LBacchiani <lorenzo.bacchiani2@unibo.it>
2023-06-19 13:49:55 +0200

    remove dockerfile + launch instructions

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # k8s_smart_deployer
+
+
+Before running the tool you need to run Zephyrus2 using the following command:
+
+```
+sudo docker pull jacopomauro/zephyrus2
+sudo docker run -d -p <PORT>:9001 --name zephyrus_container jacopomauro/zephyrus2
+```
+where <PORT> is a user-defined port
+
+To run the tool open a terminal in the root folder and execute 
+```
+python main.py ARG1 ARG2 ARG3 ARG4 ARG5
+```
+where
+
+```
+ARG1 is the kubelet reserved ram
+ARG2 is the kubelet reserved CPU
+ARG3 is the path to the components specification
+ARG4 is the VMs specification file name (it needs to be in the same path specified as ARG3)
+ARG4 is the value of the <PORT> specified after launching the zephyrus2 container
+```
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import json
 import os
+import sys
 import yaml
 import glob
 
 from optimizer import Optimizer
 
 if __name__ == '__main__':
-    path = 'annotation_examples/test1/'
+    args = sys.argv[1:]
+    kubelet_reserved_ram = int(args[0])
+    reserved_kublet_cpu = int(args[1])
+    path = args[2] #'annotation_examples/test1/'
     extension = '*.yaml'
     file_paths = glob.glob(f"{path}/{extension}")
-    vm_properties = json.load(open(path + 'vm_annotation.json')) #TODO PASS AS PARAMETER
-    kubelet_reserved_ram = 0 #TODO PASS AS PARAMETER
-    reserved_kublet_cpu = 0 #TODO PASS AS PARAMETER
+    vm_properties = json.load(open(path + args[3]))
+    port = args[4]
     components = []
     for file_path in file_paths:
         with open(file_path, 'r') as f:
             components.append(yaml.load(f, Loader=yaml.FullLoader))
-    optimizer = Optimizer(reserved_kublet_cpu, kubelet_reserved_ram, '--solver, lex-or-tools')
+    optimizer = Optimizer(reserved_kublet_cpu, kubelet_reserved_ram, port, '--solver, lex-or-tools')
     optimizer.optimize(vm_properties, components)
