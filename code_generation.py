e0ac786b1b6dc9f19ed08048738ce12abbc088c9
LBacchiani <lorenzo.bacchiani2@unibo.it>
2023-06-27 13:24:07 +0200

    fix yaml generation

--- code_generation.py
+++ code_generation.py
@@ -3,38 +3,13 @@ import os
 
 def generate_yaml(node_name, service_name, config_file, replicas):
     os.makedirs("deployments", exist_ok=True)
-    file_name = "deployments/"+ node_name + "_" +service_name + ".yaml"
-    clean_config(config_file['spec']['template']['spec']['containers'], "resources")
-    data = {
-        "apiVersion": "apps/v1",
-        "kind": "Deployment",
-        "metadata": {
-            "name": service_name
-        },
-        "spec": {
-            "selector": {
-                "matchLabels": {
-                    "app": service_name
-                }
-            },
-            "template": {
-                "metadata": {
-                    "labels": {
-                        "app": service_name
-                    }
-                },
-                "spec": {
-                    "containers": config_file['spec']['template']['spec']['containers'],
-                    "nodeName": node_name
-                }
-            },
-            "replicas": replicas
-        }
-    }
-
-    with open(file_name, "w") as file: yaml.dump(data, file)
+    file_name = "deployments/" + node_name + "_" + service_name + ".yaml"
+    config_file['spec']['replicas'] = replicas
+    clean_config(config_file['spec']['template']['spec'], "affinity")
+    clean_config(config_file['spec']['template']['spec'], "schedulerName")
+    config_file['spec']['template']['spec']['nodeName'] = node_name
+    with open(file_name, "w") as file: yaml.dump(config_file, file)
 
 def clean_config(config, to_remove):
-    for data in config:
-        if to_remove in data:
-            del data[to_remove]
+    if to_remove in config:
+        config.pop(to_remove)
